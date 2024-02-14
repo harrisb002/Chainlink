29d1766c812721f0615d37914767b45dc28f93f8
harrisb002 <harrisbe@sonoma.edu>
2024-02-14 00:04:04 -0800

    fundMe contract fund and withdraw functionality

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -8,22 +8,22 @@ import "./PriceConverter.sol";
 error FundMe__NotOwner(); // Convention to start with name of contract ect.
 
 // Accepts the funding sent by users
-contract FundMe{
+contract FundMe {
     // Type Declarations
     using PriceConverter for uint256; // Allows the use of the PriceConverter library methods on uint256
-    
-    // Notes: 
-    // constant must be assigned at compile-time 
+
+    // Notes:
+    // constant must be assigned at compile-time
     // immutable can be assigned at runtime but then cannot change
-    
+
     // State variables
     uint256 public constant MINIMUM_USD = 50 * 1 ether; // This is the min amount to pass to the contract to fund it (i.e. to be a supporter)
     address private immutable i_owner;
-    address[] private s_funders;    // Minimum of 50$ to be a funder defined above
+    address[] private s_funders; // Minimum of 50$ to be a funder defined above
     mapping(address => uint256) private s_addressToAmountFunded; // Stores the amount a certain person has funded
 
     // Address of the price feed, however this will be casted to the AggregatorV3Interface, so can be used in the Price Converter library
-    AggregatorV3Interface private s_priceFeed; 
+    AggregatorV3Interface private s_priceFeed;
 
     // Modifiers
     modifier onlyOwner() {
@@ -32,4 +32,32 @@ contract FundMe{
         _;
     }
 
+    // Get the amount sent by user in USD and make sure greater than minimum to be a supporter, 50$
+    function fund() public payable {
+        require(
+            msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD,
+            "You need to spend more ETH!"
+        );
+        // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
+        s_addressToAmountFunded[msg.sender] += msg.value;
+        s_funders.push(msg.sender);
+    }
+
+    // Allows the owner of the contract to withdraw the funds
+    function withdraw() public onlyOwner {
+        address[] memory funders = s_funders;
+        // mappings can't be in memory, sorry!
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            address funder = funders[funderIndex];
+            s_addressToAmountFunded[funder] = 0; // Clear the amount the supporters have funded
+        }
+        s_funders = new address[](0); // Clears the addresses of those who have contributed in the past once withdrawn (Not necessary)
+        // payable(msg.sender).transfer(address(this).balance); However better to use .call method
+        (bool success, ) = i_owner.call{value: address(this).balance}(""); // Send the balance of the contract to owner
+        require(success);
+    }
 }
